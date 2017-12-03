87b0965154c35066983d10c2c08f671b31998a5a
Acid Bubbles <acidbubbles.anon@gmail.com>
2017-12-03 14:53:09 -0500

    Add Widowmaker (and partial Triss) with semi-broken look at and hands on head

---
+++ Assets/Scripts/IKHoldInHands.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKHoldInHands : MonoBehaviour
+{
+	protected Animator animator;
+
+	public bool ikActive = false;
+	public Transform rightHandObj = null;
+	public Transform leftHandObj = null;
+
+	public void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void OnAnimatorIK()
+	{
+		if (!animator) return;
+		if (!ikActive)
+		{
+			ResetHand(AvatarIKGoal.RightHand);
+			animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+			animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+			return;
+		}
+
+		if (rightHandObj != null)
+			PositionHand(AvatarIKGoal.RightHand, rightHandObj);
+
+		if (rightHandObj != null)
+			PositionHand(AvatarIKGoal.LeftHand, leftHandObj);
+	}
+
+	private void ResetHand(AvatarIKGoal goal)
+	{
+		animator.SetIKPositionWeight(goal, 0);
+		animator.SetIKRotationWeight(goal, 0);
+	}
+
+	private void PositionHand(AvatarIKGoal goal, Transform target)
+	{
+		animator.SetIKPositionWeight(goal, 1);
+		animator.SetIKRotationWeight(goal, 1);
+		animator.SetIKPosition(goal, target.position);
+		animator.SetIKRotation(goal, target.rotation);
+	}
+}
---
+++ Assets/Scripts/IKLookAt.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKLookAt : MonoBehaviour
+{
+	protected Animator animator;
+
+	public bool ikActive = false;
+	public Transform lookObj = null;
+	public Transform leftEyeBone = null;
+	public Transform rightEyeBone = null;
+
+	public void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void OnAnimatorIK()
+	{
+		if (!animator) return;
+
+		if (!ikActive)
+		{
+			animator.SetLookAtWeight(0);
+			return;
+		}
+
+		if (lookObj != null)
+		{
+			animator.SetLookAtWeight(0.5f);
+			animator.SetLookAtPosition(lookObj.position);
+
+			if (leftEyeBone)
+				LookEye(leftEyeBone);
+
+			if (rightEyeBone)
+				LookEye(rightEyeBone);
+		}
+	}
+
+	private void LookEye(Transform eyeBone)
+	{
+		eyeBone.LookAt(lookObj);
+	}
+}
