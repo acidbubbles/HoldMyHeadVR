9f79bc595f9261088fd2c72675e41d8aecdf2002
Acid Bubbles <acidbubbles.anon@gmail.com>
2017-12-15 16:07:17 -0500

    Very simple character switcher

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+	private int _currentActiveCharacter = 0;
+
+	// ReSharper disable InconsistentNaming
+	public GameObject[] characters;
+	public int activeCharacter;
+	// ReSharper restore InconsistentNaming
+
+	public void Start()
+	{
+	}
+
+	public void Update()
+	{
+		if (_currentActiveCharacter != activeCharacter)
+			ChangeCharacter();
+	}
+
+	private void ChangeCharacter()
+	{
+		if (activeCharacter >= characters.Length) return;
+		characters[_currentActiveCharacter].SetActive(false);
+		_currentActiveCharacter = activeCharacter;
+		characters[_currentActiveCharacter].SetActive(true);
+	}
+}
