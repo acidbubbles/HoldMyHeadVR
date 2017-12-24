f70864b44fe96cc5b7ab6b13edb460bbfd2a5300
Acid Bubbles <acidbubbles.anon@gmail.com>
2017-12-23 23:55:21 -0500

    Better default position, improve Triss hump angle

--- Assets/Scripts/CharacterUtils/PelvisController.cs
+++ Assets/Scripts/CharacterUtils/PelvisController.cs
@@ -6,16 +6,16 @@ public class PelvisSettings : ControllerSettings
 {
 	public Vector3 HipsForward;
 	public Vector3 HipsThrustCompensation;
-	public float BodyThrustRotation;
+	public float BodyThrustRotation = 20f;
+	public float EyesToMouthDistance = 0.03f;
+	public float EyesToBellyMinDistance = 0.18f;
 }
 
 public class PelvisController
 {
-	private const float EyesToMouthDistance = 0.025f;
-	private const float EyesToBellyMinDistance = 0.18f;
-	private const float ReachDistance = 0.25f;
+	private const float ReachDistance = 0.35f;
 	private const float ReachDuration = 1.2f;
-	private const float HumpSpeed = 4f;
+	private const float HumpSpeed = 5f;
 	private const float HumpDistance = 0.12f;
 	private const float BodyUpWiggleRoom = 0.05f;
 
@@ -27,6 +27,7 @@ public class PelvisController
 
 	private Vector3 _initialBodyPosition;
 	private Quaternion _initialBodyRotation;
+	private Quaternion _initialHipsRotation;
 	private bool _ready;
 
 	public PelvisController(PelvisSettings settings, Animator animator, Transform head, Transform ground)
@@ -49,23 +50,32 @@ public class PelvisController
 		{
 			_initialBodyPosition = _animator.bodyPosition - new Vector3(0, BodyUpWiggleRoom, 0);
 			_initialBodyRotation = _animator.bodyRotation;
+			_initialHipsRotation = Quaternion.Euler(_settings.HipsForward);
 			_ready = true;
 		}
 
 		var adjustedInitialBodyPosition = _initialBodyPosition + new Vector3(0, _ground.position.y, 0);
-		var target = _head.position + _head.TransformDirection(Vector3.down) * EyesToMouthDistance;
+		var target = _head.position + _head.TransformDirection(Vector3.down) * _settings.EyesToMouthDistance;
 		var withinReach = adjustedInitialBodyPosition.y + BodyUpWiggleRoom >= target.y && Vector3.Distance(adjustedInitialBodyPosition, target) < ReachDistance;
 		var weight = _reach.GetWeight(withinReach, ReachDuration);
 
-		var humpUnit = (Mathf.Sin(Time.time * HumpSpeed) + 1) / 2f;
+		if (weight <= 0)
+		{
+			_animator.SetBoneLocalRotation(HumanBodyBones.Hips, _initialHipsRotation);
+			_animator.bodyPosition = _initialBodyPosition;
+			_animator.bodyRotation = _initialBodyRotation;
+			return;
+		}
+
+		var humpUnit = 1f - (Mathf.Sin(Time.time * HumpSpeed) + 1) / 2f;
 		var humpDistance = humpUnit * HumpDistance;
 		var weightedHumpUnit = humpUnit * weight;
-		var targetBodyPosition = _head.position + _head.TransformDirection(Vector3.forward) * (EyesToBellyMinDistance + humpDistance) + _head.TransformDirection(Vector3.down) * EyesToMouthDistance;
+		var targetBodyPosition = _head.position + _head.TransformDirection(Vector3.forward) * (_settings.EyesToBellyMinDistance + humpDistance) + _head.TransformDirection(Vector3.down) * _settings.EyesToMouthDistance;
 		_animator.bodyPosition = Vector3.Slerp(_initialBodyPosition, targetBodyPosition, weight);
-		var targetBodyRotation = Quaternion.LookRotation(-(_animator.bodyPosition - _head.position)) * Quaternion.AngleAxis((weightedHumpUnit - 0.5f) * _settings.BodyThrustRotation, Vector3.right);
+		var targetBodyRotation = Quaternion.LookRotation(-(_animator.bodyPosition - target)) * Quaternion.AngleAxis((weightedHumpUnit - 0.5f) * _settings.BodyThrustRotation, Vector3.right);
 		_animator.bodyRotation = Quaternion.Slerp(_initialBodyRotation, targetBodyRotation, weight);
 
 		// Compensate for body rotation
-		_animator.SetBoneLocalRotation(HumanBodyBones.Hips, Quaternion.Euler(_settings.HipsForward) * Quaternion.Euler(_settings.HipsThrustCompensation * weightedHumpUnit));
+		_animator.SetBoneLocalRotation(HumanBodyBones.Hips, _initialHipsRotation * Quaternion.Euler(_settings.HipsThrustCompensation * weightedHumpUnit));
 	}
 }
